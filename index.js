6335eba2fa06c5bb8adc57cc516a3cfe523d77e4
nofamex <nsiren517@gmail.com>
2022-11-27 17:18:05 +0700

    change interval

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
-const redPack = require("./data/botbikunMerah.json");
-const bluePack = require("./data/botBikunBiru.json");
+const redPack = require("./data/red.json");
+const bluePack = require("./data/blue.json");
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -18,30 +18,31 @@ function clientRunner(client, route, ms, name) {
       for (let x = 0; x < route.length; x++) {
         const data = route[x];
         const payload = {
-          long: data.long,
+          long: data.lng,
           lat: data.lat,
-          speed: data.speed,
-          heading: data.heading,
+          speed: 1.0,
+          heading: 1.0,
         };
         client.send(JSON.stringify(payload));
-        await sleep(3000);
+        await sleep(1000);
       }
     }
   });
 }
 
 async function main() {
-  var client = new WebSocket(
-    "wss://api.bikunku.com/bus/stream?type=driver&experimental=true&experimentalId=1"
-  );
-
-  var client2 = new WebSocket(
-    "wss://api.bikunku.com/bus/stream?type=driver&experimental=true&experimentalId=2"
-  );
-
-  clientRunner(client, redPack, 0, "1");
-
-  clientRunner(client2, bluePack, 5000, "2");
+  for (let index = 0; index < 4; index++) {
+    const url = `wss://api.bikunku.com/bus/stream?type=driver&experimental=true&experimentalId=${
+      index + 1
+    }`;
+    const client = new WebSocket(url);
+    clientRunner(
+      client,
+      index % 2 == 0 ? redPack : bluePack,
+      index * 30000,
+      `${index}`
+    );
+  }
 }
 
 main();
