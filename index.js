c2bbb5c0889cc289712c68963a3d4733b4da954d
nofamex <nsiren517@gmail.com>
2022-11-27 15:02:41 +0700

    change module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const W3CWebSocket = require("websocket").w3cwebsocket;
-const redPack = require("./botbikunMerah.json");
-const bluePack = require("./botBikunBiru.json");
+const WebSocket = require("ws");
+const redPack = require("./data/botbikunMerah.json");
+const bluePack = require("./data/botBikunBiru.json");
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function clientRunner(client, route, ms, name) {
-  client.onopen = async function () {
+  client.on("open", async function () {
     console.log("WebSocket Client Connected");
 
     await sleep(ms);
@@ -27,21 +27,15 @@ function clientRunner(client, route, ms, name) {
         await sleep(3000);
       }
     }
-  };
-
-  client.onmessage = function (e) {
-    if (typeof e.data === "string") {
-      console.log("Received: '" + e.data + "'");
-    }
-  };
+  });
 }
 
 async function main() {
-  var client = new W3CWebSocket(
+  var client = new WebSocket(
     "wss://api.bikunku.com/bus/stream?type=driver&experimental=true&experimentalId=1"
   );
 
-  var client2 = new W3CWebSocket(
+  var client2 = new WebSocket(
     "wss://api.bikunku.com/bus/stream?type=driver&experimental=true&experimentalId=2"
   );
 
